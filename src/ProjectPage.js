4ecd4f930dd270b34ffee96ddbeb0b591bbee7ca
Wombatlord <jordan.stone0@gmail.com>
2022-10-03 23:11:13 +0100

    Refactored properly to functional components. Nav only used for routing. Modal returned to stateful with useState hooks.

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 import { Collapse, Container, Row, Col, Stack } from "react-bootstrap";
 import Modal from "./Modal/Modal";
-import content from "./content.json";
-import all from "./assets/raw_content.json";
 import { RenderedMarkdown } from "./converter";
-import { useNavigate } from "react-router-dom";
+import { getPost } from "./Posts"
 
-function ProjectPage(props) {
-    const { show, setShow, content } = props;
-    let {currentItem} = props;
+const defaultItem = () => ({ heading: "", sections: [] })
 
-    if (!currentItem) {
-        currentItem = { heading: "", sections: [] }
+function ProjectPage({ content }) {
+    // these state variables control whether the project page shows the item in the modal
+    let [show, setShow] = useState(false);
+    let [item, setItem] = useState(defaultItem());
+
+    // this function maps the post index to a click handler that sets the item with the return value from getPost
+    // and sets show = true.
+    const handlers = (idx) => () => {
+        setItem(getPost(idx));
+        setShow(true);
     }
- 
-    const navigate = useNavigate();
-    const handlers = (idx) => () => navigate(`/show/project/${idx}`);
 
-    console.log("show", show)
     return (
         <div>
             <PageBody>
@@ -33,15 +33,14 @@ function ProjectPage(props) {
             <CurrentPost
                 showModal={show}
                 setShow={setShow}
-                heading={currentItem.heading}
-                sections={currentItem.sections}
+                post={item}
             >
             </CurrentPost>
         </div>
     )
 }
 
-
+// Collapsible is wrapper that encloses the passed children in a fold-away container, toggles on click
 function Collapsible({ children, title }) {
     const [open, setOpen] = useState(false);
 
@@ -57,29 +56,29 @@ function Collapsible({ children, title }) {
     )
 }
 
+// This allows various ways to present the content of a post section depending on the syntax
+const renderFuncs = {
+    markdown: (inner) => <RenderedMarkdown Markdown={inner}></RenderedMarkdown>,
+    default: (inner) => inner,
+}
+
 function CurrentPost(props) {
-    console.log("CurrentPost show", props.showModal);
+    // this function returns the jsx for a single section of a post
+    const renderSection = (section, key) => {
+        const sectionContent = renderFuncs[section.syntax](section.content)
+        return (<PostSection heading={section.heading} key={key} content={sectionContent} ></PostSection>)
+    }
+
     return (
         <>
-            {
-                props.sections.length === 0 ? "" : (
+            {props.post.sections.length &&
+                (
                     <Modal
                         show={props.showModal}
                         setShow={props.setShow}
-                        heading={props.heading}
+                        heading={props.post.heading}
                     >
-                        {
-                            props.sections.map(
-                                (section, key) => {
-                                    const sectionContent = section.syntax === "markdown"
-                                        ? <RenderedMarkdown Markdown={section.content}></RenderedMarkdown>
-                                        : section.content;
-
-
-                                    return (<PostSection heading={section.heading} key={key} content={sectionContent} ></PostSection>)
-                                }
-                            )
-                        }
+                        {props.post.sections.map(renderSection)}
                     </Modal>
                 )
             }
@@ -88,20 +87,20 @@ function CurrentPost(props) {
 }
 
 function ProjectList({ items, clickHandlers }) {
+    // this function returns the jsx for an item in the projects list.
+    const renderListEntry = (item, index) => (
+        <Row key={index}>
+            <MarkdownItem
+                item={item}
+                clickHandler={clickHandlers(index)}
+                left={(index % 2 === 0)}
+            />
+        </Row>
+    )
+
     return (
-        <Container
-            className="mdItem"
-        >
-            <Col>{
-                items.map((item, index) => (
-                    <Row key={index}>
-                        <MarkdownItem
-                            item={item}
-                            onClick={clickHandlers(index)}
-                            left={(index % 2 === 0)}
-                        />
-                    </Row>
-                ))}</Col>
+        <Container className="mdItem">
+            <Col>{items.map(renderListEntry)}</Col>
         </Container>
     );
 }
@@ -114,11 +113,11 @@ function PostSection({ heading, content }) {
     return (<Collapsible title={heading}>{content}</Collapsible>)
 }
 
-const MarkdownItem = ({ item, onClick, left }) => {
+const MarkdownItem = ({ item, clickHandler, left }) => {
     return (
         <Stack gap={4}>
             <Stack direction="horizontal">
-                <h2 className={(left ? "" : "ms-auto ") + (left ? "projectTitleLeft" : "projectTitleRight")} onClick={onClick}>{item.name}</h2>
+                <h2 className={(left ? "" : "ms-auto ") + (left ? "projectTitleLeft" : "projectTitleRight")} onClick={clickHandler}>{item.name}</h2>
             </Stack>
             <Stack direction="horizontal" gap={4}>
                 <Synopsis item={item} left={left} />
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import React, { useState } from "react";
-
+import React from "react";
 import ReactDOM from "react-dom/client";
-
 import MyNavBar from "./Navbar/navbar"
 import content from "./content.json";
-import rawContent from "./assets/raw_content.json";
 import ProjectPage from "./ProjectPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-
-console.log(content);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
@@ -20,45 +15,12 @@ function App() {
         <MyNavBar gitLink={"https://github.com/Wombatlord"} />
       </div>
       <Routes>
-        <Route path="/" element={<ModalDeepLink showModal={false} />} />
+        <Route path="/" element={<ProjectPage content={content}/>} />
         <Route path="/about" element={<p>about placeholder</p>} />
         <Route path="/contact" element={<p>contact placeholder</p>} />
-        <Route path="/show/project/:id" element={<ModalDeepLink showModal={true} />} />
       </Routes>
     </div>
   );
-}
-
-// getPost pulls the post content from the raw_content.json by the post id.
-const getPost = (postId) => {
-  // Here we can access assets/raw_content.json to inject the item sections into the project page as a prop
-
-  if (typeof postId !== 'string') {
-    return { currentItem: { heading: "", sections: [] } }
-  }
-
-  console.log("postId", typeof postId)
-  console.log("getPost Called")
-
-  const newModalContent = rawContent[postId];
-  const modalProps = {
-    currentItem: {
-      heading: newModalContent.name,
-      sections: [
-        {
-          heading: "Readme.md",
-          syntax: "markdown",
-          content: newModalContent.raw,
-        },
-        {
-          heading: "other",
-          content: "text"
-        }
-      ],
-    }
-  }
-  console.log("modalProps", modalProps)
-  return modalProps
 }
 
 /*
@@ -70,21 +32,6 @@ let technicallyCorrectFunctionSyntax = ModalDeepLink({prop1: 1, prop2: "2", chil
 
 assert preferredSytax === technicallyCorrectSyntax
 */
-function ModalDeepLink({ showModal }) {
-  // url path paramater access
-  let { id } = useParams();
-  console.log("id", typeof id);
-
-  const item = getPost(id).currentItem;
-
-  const [show, setShow] = useState(showModal);
-
-  console.log("showModal", showModal);
-  console.log("ModalDeepLink", item);
-  return (
-    <ProjectPage show={show} setShow={setShow} currentItem={item} content={content} />
-  )
-}
 
 // ========================================
 // This is the application bootstrap
