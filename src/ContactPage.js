ef15462c03bc406c4e2e9aa66d35c18024c58b4f
Wombatlord <jordan.stone0@gmail.com>
2022-10-07 03:25:02 +0100

    Clear email & subject fields on submission.

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -16,53 +16,76 @@ function ContactPage() {
 }
 
 function ContactForm() {
+    /*
+    This is the main component which composes the user input fields for a contact form.
+    The submitHandler is attached to the <form> element.
+    It prevents the default browser behaviour which is to refresh the page, this allows
+    us to maintain state after submission. This is used to clear the email & subject fields,
+    and to replace the user supplied message content with a string passed in setSubmitText.
+    */
+    let [emailField, setEmailField] = useState("");
+    let [subjectField, setSubjectField] = useState("");
     let [submitText, setSubmitText] = useState("");
 
     const submitHandler = (event) => {
+        // This prevents the page refreshing and losing state.
+        // Allows useState hooks to change the user submitted content on submission.
         event.preventDefault();
-        setSubmitText("Thanks for messaging!")
+        
+        // Clear email & subject.
+        setEmailField("");
+        setSubjectField("");
+        
+        // Replace email body with passed string.
+        setSubmitText("Thanks for messaging!");
     }
 
     return (
         <div class="contactContainer">
             <div class="row">
                 <div class="col align-self-center">
-                    <h1 class="text-center">Email</h1>
+                    <h1 class="text-center">Email JStone_Dev</h1>
                     {/* <!-- contact form --> */}
                     <form onSubmit={submitHandler}>
                         {/* <!-- email --> */}
                         <div class="form-group">
-                            <label for="email">Email address</label>
+                            <label for="email" style={{font: "22px Major Mono Display, monospace"}}>Email address</label>
                             <input
                                 type="email"
                                 name="email"
                                 class="form-control"
                                 id="email"
                                 placeholder="enter your email"
+                                onChange={event => setEmailField(event.target.value)}
+                                value={emailField}
+                                style={{color: "gold"}}
                             />
                         </div>
 
                         {/* <!-- subject --> */}
                         <div class="form-group">
-                            <label for="subject">Subject</label>
+                            <label for="subject" style={{font: "22px Major Mono Display, monospace"}}>Subject</label>
                             <input
                                 type="text"
                                 name="subject"
                                 class="form-control"
                                 id="subject"
                                 placeholder="enter email subject"
+                                onChange={event => setSubjectField(event.target.value)}
+                                value={subjectField}
+                                style={{color: "gold"}}
                             />
                         </div>
 
                         <div class="form-group">
-                            <label for="email_body">Message</label>
+                            <label for="email_body" style={{font: "22px Major Mono Display, monospace"}}>Message</label>
                             <textarea
                                 class="form-control"
-                                style={{color: "gold"}}
                                 id="email_body"
                                 rows="5"
                                 onChange={event => setSubmitText(event.target.value)}
                                 value={submitText}
+                                style={{color: "gold"}}
                             ></textarea>
                         </div>
 
